02380cb5fb1f7e32cd41496e55c471b16d6c3e0f
Alankrith G <100883843+harshan1996@users.noreply.github.com>
2023-07-22 23:17:00 +0530

    comments removed

--- frontend-nextjs-pages/components/Navbar.jsx
+++ frontend-nextjs-pages/components/Navbar.jsx
@@ -12,11 +12,7 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.links}>
-        {/* <input type="text" name="" id="" placeholder="search for an item" /> */}
-        {/* <Link href="/">Home</Link>
-        <Link href="#">Appointments</Link>
-        <Link href="#">Settings</Link>
-        <Link href="#">About</Link> */}
+      
       </div>
       <div className={styles.toggleMode} onClick={()=>setMode("black")}><LightModeIcon/></div>
       <div className={styles.name}> Hi, John Smith</div>
